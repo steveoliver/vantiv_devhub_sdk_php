30e448131bfd94a9718ffc61b88ff9037ab4ad87
Steve Oliver <steve@circatree.com>
2016-06-06 15:06:13 -0700

    Get test configuration from env if it is set.

--- tests/src/Configuration.php
+++ tests/src/Configuration.php
@@ -18,13 +18,23 @@ class Configuration {
    * Initializes config with a Configuration object from tests/config.ini values.
    */
   function __construct() {
-    $config = realpath(dirname(dirname(__FILE__))) . '/config.ini';
-    if (file_exists($config)) {
-      $config_array = parse_ini_file($config);
+    if (getenv('VANTIV_DEVHUB_LICENSE')) {
+      $config_array = [
+        'api_version' => 1,
+        'base_url' => 'https://apis.cert.vantiv.com',
+        'license' => getenv('VANTIV_DEVHUB_LICENSE')
+      ];
       $this->config = new TestConfiguration($config_array);
     }
     else {
-      throw new \Exception("Error: missing test config.ini. Please copy tests/example.config.ini to tests/config.ini and customize with your app's license.");
+      $config = realpath(dirname(dirname(__FILE__))) . '/config.ini';
+      if (file_exists($config)) {
+        $config_array = parse_ini_file($config);
+        $this->config = new TestConfiguration($config_array);
+      }
+      else {
+        throw new \Exception("Error: missing test config.ini. Please copy tests/example.config.ini to tests/config.ini and customize with your app's license.");
+      }
     }
   }
 
